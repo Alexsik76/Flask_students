7c51c7e6c3166a48f7e51c2438031a3a50199a50
Alexsik76 <alex@jurist.vn.ua>
2021-01-19 12:18:29 +0200

    generate data 2

--- data/data_gen.py
+++ data/data_gen.py
@@ -1,13 +1,25 @@
-from random import randint
+from random import choice
 import string
 
 
+with open('first_names.txt') as file:
+    first_names = list(file)
+
+with open('last_names.txt') as file:
+    last_names = list(file)
+
+
+print(*first_names)
+
+
 def get_group_name():
-    pass
+    letter = choice(string.ascii_lowercase)
+    number = choice(string.digits)
+    return f'{letter}{letter}{number}{number}{letter}{letter}'
 
 
 def get_user_name():
-    pass
+    return f'{choice(first_names)} {choice(last_names)}'
 
 
 def get_course_name():
