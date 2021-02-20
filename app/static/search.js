92624beea7d9e1580b012da7c2be1762e72c4a46
Alexsik76 <alex@jurist.vn.ua>
2021-02-21 00:09:36 +0200

    js not works

--- app/static/search.js
+++ app/static/search.js
@@ -1,7 +1,3 @@
-$(document).ready(function () {
-    $('#gr').on('input', function () {
-        if ($(this).val().length)
-            $(this).next('#st').prop('disabled', true).val('').removeAttr("alt"); // disable and clear other fields
-        else $(this).next('#st').prop('disabled', false);
-    }).trigger("input");
+$('#gr').click(function() {
+    $('#st').prop('disabled', true).val('');
 });
