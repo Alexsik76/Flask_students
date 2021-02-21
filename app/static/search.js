824c4a1241b1fe7aef49fab6547222e98f8a5183
Alexsik76 <alex@jurist.vn.ua>
2021-02-21 09:02:45 +0200

    js works

--- app/static/search.js
+++ app/static/search.js
@@ -1,3 +1,18 @@
-$('#gr').click(function() {
-    $('#st').prop('disabled', true).val('');
-});
+$(document).ready(function () {
+        $('#gr').on('input change', function () {
+            if ($(this).val() !== '') {
+                $('[id=st]').prop('disabled', true);
+            }
+            else {
+                $('[id=st]').prop('disabled', false);
+            }
+        });
+        $('[id=st]').on('input change', function () {
+            if ($(this).val() !== '') {
+                $('#gr').prop('disabled', true);
+            }
+            else {
+                $('#gr').prop('disabled', false);
+            }
+        });
+    });
