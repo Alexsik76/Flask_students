8872a5c00c9d5950e343d2bbc7e4fc6b7d4a66c3
Alexsik76 <alex@jurist.vn.ua>
2021-02-25 10:47:03 +0200

    removed excess js import and js code

--- app/static/search.js
+++ app/static/search.js
@@ -1,27 +1,9 @@
 $('#searchModal').on('shown.bs.modal', (function () {
-
     bootstrapValidate('#gr', 'numeric:Please only enter numeric characters!', function (isValid) {
    if (isValid) {
-       $("#gr").removeClass('has-warning').addClass('has-success');
        $('#submit').attr('disabled', false);
    } else {
-       $("#gr").addClass('has-success').removeClass('has-warning');
        $('#submit').attr('disabled', true);
    }
 });
 }));
-
-// $('#gr').on('input change', function () {
-//         if ($(this).val() !== '') {
-//             $('[id=st]').prop('disabled', true);
-//         } else {
-//             $('[id=st]').prop('disabled', false);
-//         }
-//     });
-//     $('[id=st]').on('input change', function () {
-//         if ($(this).val() !== '') {
-//             $('#gr').prop('disabled', true);
-//         } else {
-//             $('#gr').prop('disabled', false);
-//         }
-//     });
