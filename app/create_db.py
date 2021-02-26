3b8fa35d130a1cea8a25f76f60f70c6033784fe7
Alexsik76 <alex@jurist.vn.ua>
2021-02-26 22:30:42 +0200

    simple Query groups by size v2

--- app/create_db.py
+++ app/create_db.py
@@ -12,21 +12,22 @@ def init_db():
         db.drop_all()
     db.create_all()
     students, groups, courses = generate()
-    groups_db = [GroupModel(name=group) for group in groups]
-    db.session.add_all(groups_db)
     courses_db = [CourseModel(name=course) for course in courses]
     db.session.add_all(courses_db)
     students_db = []
-    for group in GroupModel.query.all():
-        for number in range(randint(10, 30)):
-            if students:
-                first_name, last_name = students.pop()
-                target_courses = sample(courses_db, randint(1, 3))
-                students_db.append(StudentModel(first_name=first_name,
-                                                last_name=last_name,
-                                                group_id=group.id,
-                                                courses=target_courses))
+    for student in students:
+        first_name, last_name = student
+        target_courses = sample(courses_db, randint(1, 3))
+        students_db.append(StudentModel(first_name=first_name,
+                                        last_name=last_name,
+                                        courses=target_courses))
     db.session.add_all(students_db)
+    groups_db = [GroupModel(name=group) for group in groups]
+    db.session.add_all(groups_db)
+    for group in groups_db:
+        for group_size in range(randint(10, 30)):
+            student = students_db.pop()
+            student.group = group
     db.session.commit()
     print('Data stored to the DB')
 
--- app/models.py
+++ app/models.py
@@ -1,17 +1,6 @@
 from app import db
 
 
-class GroupModel(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(5), index=True)
-
-    def __repr__(self):
-        return f'<Group {self.name}>'
-
-    def get_dict(self):
-        return {'name': self.name, 'size': len(self.students)}
-
-
 courses = db.Table('courses',
                    db.Column('student_id', db.Integer, db.ForeignKey('student_model.id'), primary_key=True),
                    db.Column('course_id', db.Integer, db.ForeignKey('course_model.id'), primary_key=True))
@@ -26,16 +15,26 @@ class CourseModel(db.Model):
         return f'<Student {self.name} {self.description}>'
 
 
+class GroupModel(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(5), index=True)
+    students = db.relationship('StudentModel', backref='group', lazy=True)
+
+    def get_dict(self):
+        return {'name': self.name, 'size': len(self.students)}
+
+    def __repr__(self):
+        return f'<Group {self.name}>'
+
+
 class StudentModel(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(24), index=True)
     last_name = db.Column(db.String(24), index=True)
-    group_id = db.Column(db.Integer, db.ForeignKey('group_model.id'), nullable=False)
-    group = db.relationship('GroupModel',  lazy='select',
-                            backref=db.backref('students', lazy='dynamic'))
     courses = db.relationship('CourseModel', secondary=courses, lazy='subquery',
                               backref=db.backref('students', lazy=True))
+    group_id = db.Column(db.Integer, db.ForeignKey('group_model.id'))
 
     def __repr__(self):
         return f'<Student {self.first_name} {self.last_name}\n' \
-               f'Group {self.group}\n>'
+               f'Group {self.group.name}\n>'
