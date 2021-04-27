9f55ad4bf2adc4cda857654fed76531c5797027f
Alexsik76 <alex@jurist.vn.ua>
2021-04-27 07:00:59 +0300

    added: - test db; - remove course; - delete student. Use test db in memory.

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -1,8 +1,16 @@
+from app.models import StudentModel, GroupModel, CourseModel
 
 
 def test_cli(runner):
     result = runner.invoke(args=['init-db'])
     assert 'Initialized the database.' in result.output
+
+
+def test_db(app):
+    with app.app_context():
+        assert StudentModel.query.count() == 200
+        assert GroupModel.query.count() == 10
+        assert CourseModel.query.count() == 10
 
 
 def test_index(client):
@@ -49,10 +57,25 @@ def test_api3(client):
     assert json_data['courses'] == []
 
 
-def test_process_course(client):
-    rv = client.get('/_update_courses/',
-                    data={'course': 'Law', 'student_id': '201', 'action': "append"})
-    assert rv is not None
-    assert rv.status_code == 200
+def test_process_course_add(client):
+    rv1 = client.get('/_update_courses/',
+                     data={'course': 'Law', 'student_id': '201', 'action': "append"})
+    assert rv1.status_code == 200
     rv2 = client.get("/api/v1/students/201")
     assert 'Law' in rv2.get_json()['courses']
+
+
+def test_process_course_remove(client):
+    rv1 = client.get('/_update_courses/',
+                     data={'course': 'Law', 'student_id': '201', 'action': "remove"})
+    assert rv1.status_code == 200
+    rv2 = client.get("/api/v1/students/201")
+    assert 'Law' not in rv2.get_json()['courses']
+
+
+def test_delete_student(client):
+    rv1 = client.get('/_delete_student/',
+                     data={'student_id': 201})
+    assert rv1.status_code == 200
+    rv2 = client.get("/api/v1/students/201")
+    assert rv2.status_code == 404
