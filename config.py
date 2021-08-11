185bd6770b548d64a6df503b9b66aedada1f9bcb
Alexsik76 <alex@jurist.vn.ua>
2021-08-11 12:00:08 +0000

    Fixed for deployment

--- config.py
+++ config.py
@@ -13,16 +13,10 @@ def get_env_variable(name):
         raise Exception(message)
 
 
-DB_USER = get_env_variable("DB_USER")
-DB_PW = get_env_variable("DB_PW")
-DB_NAME = get_env_variable("DB_NAME")
-DB_PATH = get_env_variable("DB_PATH")
-
-
 class Config(object):
     DEBUG = False
     TESTING = False
-    SECRET_KEY = 'dev'
+    SECRET_KEY = get_env_variable('SECRET_KEY')
     STATIC_FOLDER = 'app/static'
     TEMPLATES_FOLDER = 'app/templates'
     BASE_DIR = basedir
@@ -31,11 +25,10 @@ class Config(object):
     # BOOTSTRAP_ICON_SIZE = '1.5em'
     # BOOTSTRAP_ICON_COLOR = 'light'
     # SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(BASE_DIR, 'app.db')
-    SQLALCHEMY_DATABASE_URI = os.environ.get(
-        'DATABASE_URL') or f'postgresql+psycopg2://{DB_USER}:{DB_PW}@{DB_PATH}/{DB_NAME}'
+    SQLALCHEMY_DATABASE_URI = get_env_variable('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SESSION_COOKIE_SAMESITE = 'Lax'
-    SESSION_COOKIE_SECURE = True
+    # SESSION_COOKIE_SECURE = True
 
 
 class DevelopmentConfig(Config):
